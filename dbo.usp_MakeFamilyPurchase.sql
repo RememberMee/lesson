b17af0bfbd7ae82f7464edf2761ec8eee67fc611
RememberMee <deadpool.k@yandex.ru>
2023-08-27 10:53:04 +0400

    Changed dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -4,9 +4,14 @@ as
 	declare @id_family int;
 	set @id_family = (select ID from dbo.Family where Surname = @FamilySurName);
 
-	update 
-		dbo.Family
-	set 
-		BudgetValue  = BudgetValue - (select sum(value) from dbo.Basket where ID_Family = @id_family)
-	where
-		Surname = @FamilySurName;
+	if(@id_family is null)
+		print 'Семьи с данной фамилии нет в базе данных';
+	else
+		begin
+			update 
+				dbo.Family
+			set 
+				BudgetValue  = BudgetValue - (select sum(value) from dbo.Basket where ID_Family = @id_family)
+			where
+				Surname = @FamilySurName;
+		end;
